09658822fac7cb5074ca007af55d45f7c3377c3c
guillaume176 <guillaumer.176@gmail.com>
2025-12-01 23:34:47 +0000

    Script de récupération ifd_10_24

--- data/load_idf_10_24.py
+++ data/load_idf_10_24.py
@@ -1,11 +1,11 @@
 import pandas as pd
 
+#Récupération idf_10_24 depuis le cloud S3
 def load_base():
     url = "https://minio.lab.sspcloud.fr/guillaume176/diffusion/data_final/idf_10_24.parquet"
     data = pd.read_parquet(url,engine="pyarrow")
     print("Data Frame idf_10_24 récupéré depuis S3")
     return data
-
 
 
 
