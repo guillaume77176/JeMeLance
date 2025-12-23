f21a82e2d6a2f219e08e160b7dc09ad4372fc9d0
guillaume176 <guillaumer.176@gmail.com>
2025-12-23 03:22:22 +0000

    Ajout load_base_model

--- notebooks/data/load_idf_10_24.py
+++ notebooks/data/load_idf_10_24.py
@@ -8,4 +8,10 @@ def load_base():
     return data
 
 
+def load_base_model():
+    url = "https://minio.lab.sspcloud.fr/guillaume176/diffusion/data_model/idf_10_24MODEL.parquet"
+    data = pd.read_parquet(url,engine="pyarrow")
+    print("Data Frame idf_10_24MODEL récupéré depuis S3")
+    return data
+
 
